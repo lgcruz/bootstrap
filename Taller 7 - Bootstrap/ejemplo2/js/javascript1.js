a2ab35c637aa5278b4fe64db6a1c6df0d0e8dc33
luis gino <lgcruz@espol.edu.ec>
2017-11-06 22:32:36 -0500

    carussel

--- Taller 7 - Bootstrap/ejemplo2/js/javascript1.js
+++ Taller 7 - Bootstrap/ejemplo2/js/javascript1.js
@@ -1,38 +1,31 @@
-// JavaScript source code
-$('.responsive').slick({
-  dots: true,
-	prevArrow: $('.prev'),
-	nextArrow: $('.next'),
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+// Instantiate the Bootstrap carousel
+$(".multi-item-carousel").carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$(".multi-item-carousel .item").each(function() {
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(":first");
+  }
+  next
+    .children(":first-child")
+    .clone()
+    .appendTo($(this));
+
+  if (next.next().length > 0) {
+    next
+      .next()
+      .children(":first-child")
+      .clone()
+      .appendTo($(this));
+  } else {
+    $(this)
+      .siblings(":first")
+      .children(":first-child")
+      .clone()
+      .appendTo($(this));
+  }
 });
